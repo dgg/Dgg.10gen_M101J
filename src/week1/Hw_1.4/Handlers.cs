fa46fd599bf5dcf2c05129d1b0083f9493dbec00
Daniel González García <danielgonnet@gmail.com>
2013-07-31 15:53:17 +0200

    add solution to hw 1.4
    and reorganize project structure

---
+++ src/week1/Hw_1.4/Handlers.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Nancy;
+
+namespace Hw_1._4
+{
+	public class Handlers : NancyModule
+	{
+		public Handlers()
+		{
+			Get["/"] = _ =>
+			{
+				var client = new MongoClient("mongodb://localhost:27017");
+				MongoCollection collection = client
+					.GetServer().GetDatabase("week1")
+					.GetCollection("funnynumbers");
+
+				AggregateResult result = collection.Aggregate(
+					new BsonDocument
+					{{
+						 "$group", new BsonDocument
+						 {
+							{"_id", "$value"},
+							{"count", new BsonDocument { {"$sum", 1} }}
+						 }
+					}},
+					new BsonDocument
+					{{
+						 "$match", new BsonDocument
+						 {
+							{"count", new BsonDocument{{"$lte", 2}}}
+						 }
+					}},
+					new BsonDocument{{"$sort", new BsonDocument{{"_id", 1}}}});
+
+				double answer = result.ResultDocuments
+					.Sum(d => d["_id"].AsDouble);
+
+
+				return View["Answer.html", answer];
+			};
+		}
+	}
+}
