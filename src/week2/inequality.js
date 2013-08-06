512761e6e9dbeb6f6a46c227bdc26b5edd2dca94
Daniel González García <danielgonnet@gmail.com>
2013-08-06 14:09:01 +0200

    added document to explain inequality query  operators

---
+++ src/week2/inequality.js
@@ -0,0 +1,18 @@
+db.people.insert({name: "Alice"});
+db.people.insert({name: "Bob"});
+db.people.insert({name: "Charlie"});
+db.people.insert({name: "Dave"});
+db.people.insert({name: "Edgar"});
+db.people.insert({name: "Fred"});
+
+// people whose names are less than D: Alice bob, and Charlie
+db.people.find({name : {$lt : "D"}});
+
+// wrong, wrong, wrong way of model data
+db.people.insert({name: 42});
+// there is no way that {name: 42} will be returned from a query that compares strings
+db.people.find({name : {$lt : "D"}});
+
+// Which of the following will find all users with name between "F" and "Q"?
+db.users.find( { name : { $gte : "F" , $lte : "Q" } } );
+db.users.find( { name : { $lte : "Q" , $gte : "F" } } );
