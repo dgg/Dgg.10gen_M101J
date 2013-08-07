5b729b325f37d80102fc05759a8831f4edadbca5
Daniel González García <danielgonnet@gmail.com>
2013-08-07 10:30:36 +0200

    samples for count

---
+++ src/week2/count.js
@@ -0,0 +1,4 @@
+db.scores.count({type : "exam"});
+
+//How would you count the documents in the scores collection where the type was "essay" and the score was greater than 90?
+db.scores.count({type : "essay", score : {$gt : 90}});
