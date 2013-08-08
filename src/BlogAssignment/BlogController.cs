0c607102d4c4cdfbe74de3ce442d0c3e41984dfa
Daniel González García <danielgonnet@gmail.com>
2013-08-08 13:12:50 +0200

    added project for the blog assigment. Configured root

---
+++ src/BlogAssignment/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Dgg.tengen_M101J.BlogAssignment
+{
+	public class BlogController : NancyModule
+	{
+		public BlogController()
+		{
+			Get["/"] = _ => View["blog_template.html", new { }];
+		}
+	}
+}
