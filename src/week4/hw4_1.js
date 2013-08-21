00ef7aea10a7f55d35737970292dc39ae7659c88
Daniel González García <danielgonnet@gmail.com>
2013-08-21 08:32:17 +0200

    added queries to homework

--- src/week4/hw4_1.js
+++ src/week4/hw4_1.js
@@ -17,3 +17,9 @@ db.products.ensureIndex({price : -1});
 db.products.ensureIndex({description : 1});
 db.products.ensureIndex({category : 1, brand : 1});
 db.products.ensureIndex({'reviews.author': 1});
+
+// queries
+db.products.find({'brand':"GE"}).explain();
+db.products.find({'brand':"GE"}).sort({price:1}).explain();
+db.products.find({$and:[{price:{$gt:30}},{price:{$lt:50}}]}).sort({brand:1}).explain();
+db.products.find({brand:'GE'}).sort({category:1, brand:-1}).explain();
