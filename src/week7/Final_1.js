03a82c11f7041e436bcfc649143ad513e4148022
Daniel González García <danielgonnet@gmail.com>
2013-09-10 20:37:15 +0200

    add text to final 1

--- src/week7/Final_1.js
+++ src/week7/Final_1.js
@@ -1,3 +1,14 @@
+/*
+
+In this dataset, each document is an email message. Like all Email messages, there is one sender but there can be multiple recipients.
+
+Construct a query to calculate the number of messages sent by Andrew Fastow, CFO, to Jeff Skilling, the president. Andrew Fastow's email addess was andrew.fastow@enron.com. Jeff Skilling's email was jeff.skilling@enron.com.
+
+For reference, the number of email messages from Andrew Fastow to John Lavorato (john.lavorato@enron.com) was 1. 
+
+*/
+
+
 use enron;
 
 assert(db.messages.count() === 120477);
